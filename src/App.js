dee6fb756b361d32e46ad1e4310a4dbe35a5dc21
Denys <ulvanskyidenys@gmail.com>
2021-08-14 18:29:28 +0300

    implement logic og getting current weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Display from './components/Display/Display';
 
-console.log(process.env.REACT_APP_WEATHER_API_KEY)
+const KEY_CURRENT = process.env.REACT_APP_API_KEY
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState([])
+
+  const [latitude, setLatitude] = useState([])
+  const [longitude, setLongitude] = useState([])
+
+  useEffect(()=>{
+    const getCurrentWeather = async () =>{
+      navigator.geolocation.getCurrentPosition(position=>{
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+      })
+
+      await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY_CURRENT}`)
+      .then(res=>res.json())
+      .then(res=>setCurrentWeather(res))
+    }
+
+    getCurrentWeather()
+  },[latitude, longitude])
+
+  console.log(currentWeather)
+
   return (
     <div className="App">
       <Display />
