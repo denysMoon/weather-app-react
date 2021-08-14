ca511fcfa899aa9f7c6e967e3b19b66cd5d31c2e
Denys <ulvanskyidenys@gmail.com>
2021-08-14 22:57:41 +0300

    progressing of display's block

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -8,6 +8,14 @@ const Display = ( { currentWeather } ) =>{
                     <p>Lviv</p>
                     <button className="button-icon"><img className="icon" src={reload} alt="refresh" title="Reload" /></button>
                 </div>
+                <div className="date">
+                    <p>Temperature: &#8451;</p>
+                    <p>Feels like: &#8451;</p>
+                    <p>Humidity: </p>
+                    <p>Wind: m/s</p>
+                    <p>Sunrise: </p>
+                    <p>Sunset: </p>
+                </div>
             </div>
         </div>
     )
