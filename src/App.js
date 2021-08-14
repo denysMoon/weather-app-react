50dce00423b0f92a86e7a060734e5afcd3207b9a
Denys <ulvanskyidenys@gmail.com>
2021-08-14 17:49:06 +0300

    create .env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Display from './components/Display/Display';
 
+console.log(process.env.REACT_APP_WEATHER_API_KEY)
+
 function App() {
   return (
     <div className="App">
